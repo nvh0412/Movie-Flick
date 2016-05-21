f148e60857a4a503afee07f122fb37cf5d4b3a44
Hoa Nguyen <nvh0412@gmail.com>
2016-05-21 11:38:57 +0700

    Add Readme File and add tableview

--- FlickMovie/ViewController.swift
+++ FlickMovie/ViewController.swift
@@ -8,18 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var movieTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        movieTableView.dataSource = self
+        movieTableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = movieTableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath)
+        cell.textLabel!.text = "Row \(indexPath.row)"
+        print("Row \(indexPath.row)")
+        return cell
+    }
 
 }
 
