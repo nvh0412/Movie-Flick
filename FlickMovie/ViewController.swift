6a5a2d734ff2c2619e67c83c6d3a00117ff8259d
Hoa Nguyen <nvh0412@gmail.com>
2016-05-21 10:43:28 +0700

    [f] refactor structure

---
+++ FlickMovie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickMovie
+//
+//  Created by Hòa Nguyễn Văn on 5/21/16.
+//  Copyright © 2016 SkyUnity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
